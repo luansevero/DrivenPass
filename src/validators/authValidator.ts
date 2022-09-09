86c8387d8f473378126f864c112c376d97f06148
Luan Severo <luansevero@icloud.com>
2022-09-09 13:34:17 -0300

    [Feat] New Account validation

--- src/validators/authValidator.ts
+++ src/validators/authValidator.ts
@@ -1,2 +1,8 @@
 import * as authRepository from "../repositories/authRepository";
+import { AuthData } from "../types/authTypes";
+import { ErrorInfo } from "../middlewares/errorHandlerMiddleware";
 
+export async function newAccount(email: string){
+    const account : AuthData = await authRepository.findByEmail(email);
+    if(account) throw new ErrorInfo("error_conflict", "This email is already in use");
+};
