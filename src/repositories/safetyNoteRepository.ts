08d77f6e83446cd2e4a7ec6ec2fdc2a5e89693af
Luan Severo <luansevero@icloud.com>
2022-09-09 17:48:11 -0300

    [Feat] SafetyNotes Repository

--- src/repositories/safetyNoteRepository.ts
+++ src/repositories/safetyNoteRepository.ts
@@ -1,36 +1,35 @@
 import { prisma } from "../config/database";
-// import { CreateCredentialData, DuplicateCredentials, CredentialData } from "../types/credentialTypes";
+import { CreateSafetyNoteData } from "../types/safetyNoteTypes";
 
-// export async function findByTitleAndUserId(userId_title : DuplicateCredentials){
-//     return await prisma.credentials.findUnique({
-//         where:{userId_title}
-//     })
-// };
+export async function findByTitle(title : string){
+    return await prisma.safetyNotes.findUnique({
+        where : {title}
+    })
+};
 
-// export async function insert(createCredentialData:CredentialData){
-//     await prisma.credentials.create({
-//         data:createCredentialData
-//     })
-// }
+export async function insert(createSafetyNoteData:CreateSafetyNoteData){
+    await prisma.safetyNotes.create({
+        data:createSafetyNoteData
+    })
+}
 
-// export async function findAll(){
-//     return await prisma.credentials.findMany({
-//         select : {
-//             id: true,
-//             title: true
-//         }
-//     })
-// }
+export async function findAll(){
+    return await prisma.safetyNotes.findMany({
+        select : {
+            id: true,
+            title: true
+        }
+    })
+}
 
-// export async function findById(id:number){
-//     console.log(id)
-//     return await prisma.credentials.findUnique({
-//         where : {id}
-//     })
-// };
+export async function findById(id:number){
+    return await prisma.safetyNotes.findUnique({
+        where : {id}
+    })
+};
 
-// export async function deleteOne(id:number){
-//     await prisma.credentials.delete({
-//         where: {id}
-//     })
-// };
+export async function deleteOne(id:number){
+    await prisma.safetyNotes.delete({
+        where: {id}
+    })
+};
