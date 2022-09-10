bf1807ce95ba4c1fe0a653221cf4f7a114b6b935
Luan Severo <luansevero@icloud.com>
2022-09-10 13:28:42 -0300

    [Feat] Network Controller

--- src/controllers/networkController.ts
+++ src/controllers/networkController.ts
@@ -1,31 +1,31 @@
-// import { Request, Response } from 'express';
+import { Request, Response } from 'express';
 
-// import { CreateCardData } from '../types/cardTypes';
-// import * as networkService from "../services/networkService"
+import { CreateNetworkData} from '../types/networkTypes';
+import * as networkService from "../services/networkService"
 
-// export async function create(req:Request, res:Response){
-//     const { userId }  = res.locals
-//     const { title, name, password } : CreateCardData = req.body;
+export async function create(req:Request, res:Response){
+    const { userId }  = res.locals
+    const { title, name, password } : CreateNetworkData = req.body;
 
-//     await networkService.create(userId, {title, name, password});
-//     res.sendStatus(201);
-// }
+    await networkService.create(userId, {title, name, password});
+    res.sendStatus(201);
+}
 
-// export async function getAll(req:Request, res:Response){
-//     const allCards = await networkService.getAll();
-//     res.status(200).send(allCards);
-// }
+export async function getAll(req:Request, res:Response){
+    const allNetworks = await networkService.getAll();
+    res.status(200).send(allNetworks);
+}
 
-// export async function getOne(req:Request, res:Response){
-//     const { userId } = res.locals;
-//     const { id } = req.params;
-//     const card = await networkService.getOne(userId, Number(id))
-//     res.status(200).send(card);
-// }
+export async function getOne(req:Request, res:Response){
+    const { userId } = res.locals;
+    const { id } = req.params;
+    const network = await networkService.getOne(userId, Number(id))
+    res.status(200).send(network);
+}
 
-// export async function deleteOne(req:Request, res:Response){
-//     const { userId } = res.locals;
-//     const { id } = req.params;
-//     await networkService.deleteOne(userId, Number(id));
-//     res.sendStatus(200);
-// }
+export async function deleteOne(req:Request, res:Response){
+    const { userId } = res.locals;
+    const { id } = req.params;
+    await networkService.deleteOne(userId, Number(id));
+    res.sendStatus(200);
+}
