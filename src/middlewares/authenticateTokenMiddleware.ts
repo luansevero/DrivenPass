789222742cf2802ea92670368b52591f5468b18c
Luan Severo <luansevero@icloud.com>
2022-09-11 12:33:56 -0300

    [Feat] Adding a better protection to the database

--- src/middlewares/authenticateTokenMiddleware.ts
+++ src/middlewares/authenticateTokenMiddleware.ts
@@ -2,11 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 import dotenv from "dotenv";
 import jwt from 'jsonwebtoken';
 import { ErrorInfo } from './errorHandlerMiddleware';
+import { Users } from '@prisma/client';
+import * as authRepository from "../repositories/authRepository"
 
 
 dotenv.config();
 
-export default function authenticateToken(req:Request, res:Response, next:NextFunction){
+export default async function authenticateToken(req:Request, res:Response, next:NextFunction){
     const authHeader : string | string[] = req.headers["authorization"];
     if(!authHeader) throw new ErrorInfo("error_unauthorized", "Token must be send it");
     if(authHeader.split(" ")[0] !== "Bearer") throw new ErrorInfo("error_unauthorized", "Invalid token format");
@@ -17,7 +19,9 @@ export default function authenticateToken(req:Request, res:Response, next:NextFu
     let decoded = {};
     const secretToken = process.env.ACESS_SECRET_TOKEN;
     try{
-        decoded = jwt.verify(token, secretToken)
+        decoded = jwt.verify(token, secretToken);
+        const user : Users = await authRepository.findById(Number(decoded["id"]))
+        if(!user) throw new ErrorInfo("error_forbidden", "That token is expired!")
         res.locals.userId = Number(decoded["id"])
         next();
     } catch (erro){
--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,6 +1,12 @@
 import { prisma } from "../config/database";
 import { CreateAuthData } from "../types/authTypes";
 
+export async function findById(id: number){
+    return await prisma.users.findUnique({
+        where: {id}
+    })
+}
+
 export async function findByEmail(email: string){
     return await prisma.users.findUnique({
         where: {email}
